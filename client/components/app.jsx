2631a4d5d9436355976ab0c9e6765feecda5d158
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-04-24 11:25:27 -0700

    (1) client/components/app.jsx - defined App class in app.jsx with no initisal state for now. (2) client/components/header.jsx - created stateless Header function component in created client/components/header.jsx and added it to App's render method

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './header';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,17 +10,18 @@ export default class App extends React.Component {
     };
   }
 
-  componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
-  }
+  // componentDidMount() {
+  //   fetch('/api/health-check')
+  //     .then(res => res.json())
+  //     .then(data => this.setState({ message: data.message || data.error }))
+  //     .catch(err => this.setState({ message: err.message }))
+  //     .finally(() => this.setState({ isLoading: false }));
+  // }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message}</h1>;
+    return (
+      <Header />
+
+    );
   }
 }
---
+++ client/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header(props) {
+  return (
+    <nav className='mt-2 mb-2 navbar bg-dark'>
+      <div className="ml-4 navbar-brand">
+        <i className=" fas fa-dollar-sign"></i>
+        <span className="ml-1">Wicked Sales</span>
+      </div>
+    </nav>
+  );
+}
