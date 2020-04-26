e38d412c5353749f1d4748d1d7d4acdc4784c5ef
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-04-26 15:58:30 -0700

    (1) product-details.jsx - modified componentDidMount() method of ProductDetails Class component to GET specifically clicked product data. (2) styles.css - took out product-details class styling so that ProductDetails component image tag is sized correctly.

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -10,7 +10,7 @@ export default class ProductDetails extends React.Component {
   }
 
   componentDidMount() {
-    fetch('/api/products/1')
+    fetch(`/api/products/${this.props.params.productId}`)
       .then(response => response.json())
       .then(data => {
         this.setState({
