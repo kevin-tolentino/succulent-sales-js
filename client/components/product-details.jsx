aeae9dfdfe44fdb872cc39c40332005df50211ef
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-04-25 22:05:53 -0700

    client/components/product-details.jsx - defined componentDidMount() method in ProductDetails class to prepare to recieve specific product from GET

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -9,6 +9,15 @@ export default class ProductDetails extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch('/api/products/1')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ product: data });
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
     return null;
   }
