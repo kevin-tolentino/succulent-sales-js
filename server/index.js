3f905051ddb4f0bfbc1d9f2f51dba18fd910c591
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-04-28 20:50:22 -0700

    server/index.js - added new GET endpoint for "carts" table on line 65-80

--- server/index.js
+++ server/index.js
@@ -62,6 +62,23 @@ app.get('/api/products/:productId', (req, res, next) => {
 }
 );
 
+app.get('/api/cart', (req, res, next) => {
+  const sql = `
+select
+"cartId",
+"createdAt"
+from
+  "carts"
+order by
+  "cartId"
+  `;
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => next(err));
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
