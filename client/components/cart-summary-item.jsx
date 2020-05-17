aab01cda8540c118cf424e47776ca2c663ac317b
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-16 22:25:01 -0700

    added cart-summary-items components to CartSummary component with image, name, price, and short description passed down to each item using map method on cart state array and props

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -4,19 +4,17 @@ export default function CartSummaryItem(props) {
 
   function handleClick() {
     const paramObj = { productId: props.productId };
-    props.onClick('details', paramObj);
+    props.onClick('catalog', paramObj);
     // change to correct arguments ^^
   }
 
   return (
-    <div className="row">
-      <div onClick={handleClick} className="">
-        <img className="col-4" src={props.image} alt="product name" />
-        <div className="col-8">
-          <h5 className="card-title">{props.name}</h5>
-          <h6 className="card-title text-muted">${(props.price / 100).toFixed(2)}</h6>
-          <p className="text-body">{props.shortDescription}</p>
-        </div>
+    <div className="mb-2 row shadow border border-secondary">
+      <img className="p-1 col-4" src={props.image} alt="product name" />
+      <div className="col-8">
+        <h5 className="card-title">{props.name}</h5>
+        <h6 className="card-title text-muted">${(props.price / 100).toFixed(2)}</h6>
+        <p className="text-body">{props.shortDescription}</p>
       </div>
     </div>
   );
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import CartSummaryItem from './cart-summary-item';
 
 export default function CartSummary(props) {
-  return (
-    <div className="col">
-      <h4>My Cart</h4>
-    </div>
+  const reactElementArray = props.cart.map(currentIndex => {
+    return (
+      <CartSummaryItem image={currentIndex.image}
+        name={currentIndex.name}
+        price={currentIndex.price}
+        shortDescription={currentIndex.shortDescription}
+        key={currentIndex.cartItemId}/>
+    );
+  });
 
+  return (
+    <>
+      <div className="col">
+        <h4>My Cart</h4>
+      </div>
+      {reactElementArray}
+    </>
   );
 }
