37d7835eb88fd28ea90ddcb59859fee326d3d19d
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-19 15:03:05 -0700

    began defining CheckoutForm component

---
+++ client/components/checkout-form.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      creditCard: '',
+      shippingAddress: ''
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleCreditCardChange = this.handleCreditCardChange.bind(this);
+    this.handleShippingAddressChange = this.handleShippingAddressChange.bind(this);
+
+  }
+
+  handleNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleCreditCardChange(event) {
+    this.setState({ creditCard: event.target.value });
+  }
+
+  handleShippingAddressChange(event) {
+    this.setState({ shippingAddress: event.target.value });
+  }
+
+  render() {
+    return (
+      <h1>hi</h1>
+    );
+  }
+}
